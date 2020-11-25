07b8f4e2ecd4b6b57b3b79f9720879d4b1ab6ac5
EwertonBello <ewerton.bello1212@gmail.com>
2020-11-25 00:33:27 -0300

    home page created

---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Footer } from "svelte-materialify";
+</script>
+
+<style lang="scss">
+    #footer {
+        position: relative;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
+
+<div id="footer">
+    <Footer>
+        <div class="pa-4 text-center" style="width: 100%">
+            <img src="logo-192.png" alt="Svelte Materialify" width="64px" />
+            <div class="mt-2">
+                <p class="mt-1">Copyright © CadBookSvelte 2020</p>
+            </div>
+        </div>
+    </Footer>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+	import {
+        Container,
+        Button,
+        Icon,
+        Row,
+        Col,
+    } from "svelte-materialify";
+	import book_lover from 'images/book_lover.svg';
+    import Footer from '../components/footer/Footer.svelte';
 	import Toolbar from '../components/toolbar/Toolbar.svelte';
 
 </script>
@@ -8,20 +16,50 @@
 	<title>CadBook - Svelte</title>
 </svelte:head>
 
-<style>
-	img {
-		width: 200px;
-	}
+<style lang="scss">
+    .title {
+        font-size: 1.25rem;
+        font-weight: 300;
+    }
+    #home {
+        background-image: linear-gradient(135deg, #6e45e2 0%, #88d3ce 100%);
+        img {
+            width: 300px;
+        }
+    }
 </style>
 
 <Toolbar title="Home"/>
 <div class="container">
-	<h1>Great success!</h1>
-	
-	<figure>
-		<img alt="Success Kid" src="{successkid}">
-		<figcaption>Have fun with Sapper!</figcaption>
-	</figure>
-	
-	<p><strong>It is okay.</strong></p>
+	<div class="theme--dark" id="home">
+		<Container class="pt-6 pb-6">
+			<Row>
+				<Col class="d-flex justify-center flex-column" md="7">
+					<h1 class="mb-4 text-h2 text-md-h1">
+						CadBook
+					</h1>
+					<h3>
+						Crie títulos imaginários
+					</h3>
+					<p class="mb-3 title grey-text text-lighten-4">
+						Aqui sua imaginação toma forma, crie títulos de livros, 
+						diga quantas páginas esse livro terá e por quanto você pretende vende-lo. 
+						Deixe sua imaginação correr solta!!
+					</p>
+					<div class="d-flex flex-wrap">
+						<a href="/books/" class="text-decoration-none text-darken-2">
+							<Button size="large" class="primary-color">
+								<Icon class="mdi mdi-book pr-3"/>
+								COMEÇAR
+							</Button>
+						</a>
+					</div>
+				</Col>
+				<Col class="d-flex justify-center align-center flex-column" md="5">
+					<img src="{ book_lover }" alt="Girl reading" />
+				</Col>
+			</Row>
+		</Container>
+	</div>
+	<Footer/>
 </div>
